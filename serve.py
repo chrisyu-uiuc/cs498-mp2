d531e0fea12105543160737430c0832d229f8a5d
hmyu2 <hmyu2@illinois.edu>
2024-01-20 05:16:46 +0000

    new file:   serve.py

---
+++ serve.py
@@ -0,0 +1,20 @@
+#!flask/bin/python
+from flask import Flask
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return socket.gethostname()  #"Hello, World!"
+
+@app.route('/', methods=['POST'])
+def indexPost():
+    subprocess.Popen(["python", "stress_cpu.py"])
+    return "";
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
+
+
