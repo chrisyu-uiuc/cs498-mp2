618f60c534525a3995c18d6c5e54d8cac8ab5c27
yuhoming1990 <yuhoming1990@gmail.com>
2024-01-20 16:25:28 +0800

    commit

--- serve.py
+++ serve.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request
 import socket
 import subprocess
 
@@ -13,6 +13,15 @@ def index():
 def indexPost():
     subprocess.Popen(["python", "stress_cpu.py"])
     return "";
+
+@app.route('/post_json', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        return json
+    else:
+        return 'Content-Type not supported!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
